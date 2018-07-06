afefb33e56a7a78a227b69a940e7120cc86fc5a9
Pau Ruiz i Safont <psafont@ebi.ac.uk>
2018-07-06 10:04:34 +0100

    maintenance: use flask theme for docs

--- docs/conf.py
+++ docs/conf.py
@@ -37,6 +37,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinx.ext.napoleon',
+    'sphinxcontrib.fulltoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -64,7 +65,7 @@ language = None
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'flasky'
 
 
 # -- Options for extensions --------------------------------------------------
@@ -89,7 +90,7 @@ napoleon_use_rtype = False  # More legible
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'flask'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
