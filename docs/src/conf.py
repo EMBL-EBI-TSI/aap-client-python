50ce9ac66cb08446523a7f5404fc2fa3fd1edcf7
Pau Ruiz i Safont <psafont@ebi.ac.uk>
2018-07-06 08:39:57 +0100

    mainteance: use alabaster as theme

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -96,7 +96,7 @@ napoleon_use_rtype = False  # More legible
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
